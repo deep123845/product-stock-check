7e87e60f5f7c833201d614be6c4bb704fac27c24
Deep123845 <deep123845@gmail.com>
2025-05-21 20:13:28 -0400

    updated config for images

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,12 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
   output: 'export',
   images: {
     unoptimized: true
   },
-  basePath: isProd ? '/product-stock-check' : '',
-  assetPrefix: isProd ? '/product-stock-check/' : '',
+  basePath: '/product-stock-check',
+  assetPrefix: '/product-stock-check/',
 };
 
 export default nextConfig;
