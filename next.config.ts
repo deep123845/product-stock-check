b729e759550597bb604f5be6e01d2de193b6a2e2
Deep123845 <deep123845@gmail.com>
2025-05-21 20:01:19 -0400

    added github pages config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true
+  },
+  basePath: isProd ? '/product-stock-check' : '',
+  assetPrefix: isProd ? '/product-stock-check/' : '',
 };
 
 export default nextConfig;
